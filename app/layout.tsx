a2137fe337b84e2497c8bc9cbd93a6706f04e590
unknown <nsart75@gamil.com>
2026-08-28 11:20:50 +0330

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,17 @@ import "./globals.css";
 import Navbar from "@/public/src/component/Base/Navbar/Navbar";
 
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({ children }:any) {
   return (
     <html
       lang="en"
       className={` h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}
+      <body className="min-h-full flex flex-col">
         <Navbar />
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
