3f680d7bb097ce1311e12e086ce8313193761689
Apple <apple@Apples-MacBook-Air.local>
2026-08-28 04:11:25 -0700

    created api

---
+++ public/src/services/api.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3001",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const getRequest = async <T>(url: string) => {
+  const response = await api.get<T>(url);
+  return response.data;
+};
+
+export const postRequest = async <T, D>(url: string, data: D) => {
+  const response = await api.post<T>(url, data);
+  return response.data;
+};
